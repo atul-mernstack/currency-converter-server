ebde4a3b06515b2645482228ba0e9d5bd7338f24
Atul Verma <atul.verma@valuematrix.ai>
2024-01-02 19:44:02 +0530

    push

--- controller/currencyController.js
+++ controller/currencyController.js
@@ -2,15 +2,7 @@ const axios=require("axios");
 exports.getTopHundredCryptocurrencies=async(req,res)=>{
 
     try {
-        const data = await axios.get('https://api.coingecko.com/api/v3/coins/markets', {
-          params: {
-            vs_currency: 'usd',
-            order: 'market_cap_desc',
-            per_page: 100,
-            page: 1,
-            sparkline: false,
-          },
-        });   
+        const data = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');   
         const top100 = data?.data?.map((crypto) => ({
           id:crypto?.id,
           name: crypto?.name,
@@ -18,7 +10,6 @@ exports.getTopHundredCryptocurrencies=async(req,res)=>{
           current_price: crypto?.current_price,
           market_cap: crypto?.market_cap,
         }));
-    
         res.status(200).json(top100);
       } catch (error) {
         console.error(error);
