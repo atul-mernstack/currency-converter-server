9cf74683aaeb48de8905d92b2edbdd6c48cede21
Atul Verma <atul.verma@valuematrix.ai>
2024-01-03 10:15:37 +0530

    push

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-
+const path=require("path");
 const cors=require("cors");
 app.use(cors(
     {
@@ -15,11 +15,17 @@ app.use(express.json());
 
 app.use('/api',currencyApi);
 
+//app.get("*",(req,res)=>{
+//    res.status(201).json({
+//        success: true,
+//        message:"Currency Conversion Api Working fine!"
+//    });
+//})
+//initalized frontend
+app.use(express.static(path.join(__dirname,'./client/build')));
+
 app.get("*",(req,res)=>{
-    res.status(201).json({
-        success: true,
-        message:"Currency Conversion Api Working fine!"
-    });
+    res.sendFile(path.resolve(__dirname,'./client/build/index.html'))
 })
 
 
